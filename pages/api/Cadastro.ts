4d820a2af78f9caf9d8c4eb619f8593e6fac0207
JulianedePaula <112638979+JulianedePaula@users.noreply.github.com>
2023-03-15 13:21:27 -0300

    Incluindo api de imagens

--- pages/api/Cadastro.ts
+++ pages/api/Cadastro.ts
@@ -4,38 +4,52 @@ import type { RespostaPadraoMSG } from "@/Types/RespostaPadraoMSG";
 import { UsuarioModel } from "@/models/UsuarioModel";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 import md5 from "md5";
+import { updload, uploadImagemCosmic } from '../../services/uploadImagemCosmic'
+import nc from 'next-connect'
 
-const endpointCadastro = 
-    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMSG>) => {
+const handler = nc()
+    .use(updload.single('file'))
+    .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMSG>) => {
 
-        if(req.method === 'POST'){
-            const usuario = req.body as CadastroRequisicao
 
-            if(!usuario.nome || usuario.nome.length < 2){
-                return res.status(400).json({erro:'Nome de usuario invalido.'})
-            }
-            //Cadastrando e-mail
-            if(!usuario.email || usuario.email.length < 5){
-                return res.status(400).json({erro:'E-mail de usuario invalido.'})
-            }
-            //Verificando se não tem o mesmo e-mail cadastrado.
-            const usuarioDuplicidade = await UsuarioModel.find({email : usuario.email})
-            if(usuarioDuplicidade && usuarioDuplicidade.length > 0){
-                return res.status(400).json({erro: 'Email já cadastrado.'})
-            }
-            //Cadastrando a senha
-            if(!usuario.senha || usuario.senha.length < 0){
-                return res.status(400).json({erro:'Senha de usuario invalido.'})
-            }
-            const UsuarioASerSalvo = {
-                nome : usuario.nome,
-                email : usuario.email,
-                senha : md5(usuario.senha)
-            }
-            await UsuarioModel.create(UsuarioASerSalvo)
-            return res.status(200).json({msg: 'Usuario cadastrado com sucesso.'})
+        const usuario = req.body as CadastroRequisicao
 
-        } return res.status(405).json({erro: 'Metodo incorreto.'})
+        if (!usuario.nome || usuario.nome.length < 2) {
+            return res.status(400).json({ erro: 'Nome de usuario invalido.' })
+        }
+        //Cadastrando e-mail
+        if (!usuario.email || usuario.email.length < 5) {
+            return res.status(400).json({ erro: 'E-mail de usuario invalido.' })
+        }
+        //Verificando se não tem o mesmo e-mail cadastrado.
+        const usuarioDuplicidade = await UsuarioModel.find({ email: usuario.email })
+        if (usuarioDuplicidade && usuarioDuplicidade.length > 0) {
+            return res.status(400).json({ erro: 'Email já cadastrado.' })
+        }
+        //Cadastrando a senha
+        if (!usuario.senha || usuario.senha.length < 0) {
+            return res.status(400).json({ erro: 'Senha de usuario invalido.' })
+        }
+        //enviar a imagem do multer para o cosmic
+        const image = await uploadImagemCosmic(req)
+
+        //Salvar no banco de dados
+        const UsuarioASerSalvo = {
+            nome: usuario.nome,
+            email: usuario.email,
+            senha: md5(usuario.senha),
+            avatar: image?.media?.url
+        }
+        await UsuarioModel.create(UsuarioASerSalvo)
+        return res.status(200).json({ msg: 'Usuario cadastrado com sucesso.' })
+
+    })
+
+export const config = {
+    api: {
+        bodyParser: false
     }
+}
 
-export default(conectarMongoDB(endpointCadastro))
+
+export default (conectarMongoDB(handler))
---
+++ services/uploadImagemCosmic.ts
@@ -0,0 +1,38 @@
+import multer from "multer";
+import cosmicjs from "cosmicjs";
+
+const {
+    CHAVE_GRAVACAO_AVATARES,
+    CHAVE_GRAVACAO_PUBLICACOES,
+    BUCHET_AVATARES,
+    BUCHET_PUBLICACOES,
+} = process.env
+
+const Cosmic = cosmicjs()
+const bucketAvatares = Cosmic.bucket ({
+    slug: BUCHET_AVATARES,
+    write_key: CHAVE_GRAVACAO_AVATARES,
+})
+
+const bucketPublicacoes = Cosmic.bucket ({
+    slug: BUCHET_PUBLICACOES,
+    write_key: CHAVE_GRAVACAO_PUBLICACOES
+})
+
+const storage = multer.memoryStorage()
+const updload = multer({storage: storage})
+
+const uploadImagemCosmic = async (req: any) => {
+    if(req?.file?.originalname) {
+        const media_object = {
+            originalname: req.file.originalname,
+            buffer: req.file.buffer
+        }
+        if(req.url && req.url.includes('publicacao')){
+            return await bucketPublicacoes.addMedia({media: media_object})
+        }else{
+            return await bucketAvatares.addMedia({media: media_object})
+        }
+    }
+}
+export {updload, uploadImagemCosmic}
